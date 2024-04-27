1170d8737518378049b3f7db403bc0cef287e061
LTNEW52 <dereonyu27@gmail.com>
2024-04-28 03:53:19 +0600

    Changed the random password generator project , its much better now!

--- GenerateRandomPassword.js
+++ GenerateRandomPassword.js
@@ -1,30 +1,38 @@
-const passlength = 16;
-const hasLowerCase = true;
-const hasUpperCase = true;
-const hasNumber = true;
-const hasSymbol = true;
+const passlength = document.getElementById("passlength");
+const lower = document.getElementById("lower");
+const upper = document.getElementById("upper");
+const number = document.getElementById("number");
+const symbol = document.getElementById("symbol");
+const generate = document.getElementById("generate");
+const result = document.getElementById("result");
+
 
 const lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
 const upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-const number = ["0123456789"];
-const symbol = ["!@#%^&*+-*/"];
+const numberArray = ["0123456789"];
+const symbolArray = ["~!@#$%^&*_+-*/"];
 
-function generatePassword (passlength , hasLowerCase , hasUpperCase , hasNumber , hasSymbol) {
-    let choice = [];
-    choice += hasLowerCase ? lowerCase : "";
-    choice += hasUpperCase ? upperCase : "";
-    choice += hasNumber ? number : "";
-    choice += hasSymbol ? symbol : ""; 
+function generatePass (passlength , lower , upper , number , symbol) {
+    let check = [];
+    check += lower.checked ? lowerCase : "";
+    check += upper.checked ? upperCase : "";
+    check += number.checked ? numberArray : "";
+    check += symbol.checked ? symbolArray : "";
 
     let password = [];
-
-    for (let i = 0 ; i < passlength ; i++) {
-        password += choice[Math.floor(Math.random() * choice.length)];
+    for(let i = 0 ; i < passlength.value ; i++) {
+        let indexof = Math.floor(Math.random() * check.length + 1);
+        password[i] = check[indexof];
     }
-
+    console.log(password);
     return password;
 }
 
-let generatedPassword = generatePassword(passlength , hasLowerCase , hasUpperCase , hasNumber , hasSymbol);
-document.write(generatedPassword);
-
+generate.onclick = function () {
+    let generatepassword = generatePass(passlength , lower , upper , number , symbol);
+    if (generatepassword.join("") != "") {
+        result.textContent = generatepassword.join("");
+    } else {
+        result.textContent = "You have to select atleast one!";
+    }
+}
